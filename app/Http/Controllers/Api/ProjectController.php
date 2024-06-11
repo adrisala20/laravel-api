5570c0da2a75a0196418c36067027d47a288dab3
adrisala20 <adriana_15_tuya@hotmail.com>
2024-06-11 15:16:12 +0200

    cloned repository and added changes

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+Use Illuminate\Support\Facades\Auth;
+
+class ProjectController extends Controller
+{
+    public function index()
+{
+    
+    $projects = Project::all();
+    //$projects = Project::paginate();
+    return response()->json([
+        'status' => 'success',
+        'message' => 'ok',
+        'results'=> $projects
+    ], 200);
+}
+public function show($slug)
+{
+    $project = Project::where('slug', $slug)->with('category')->first() ;
+    if($project){
+        return response()->json([
+            'status' => 'success',
+            'message' => 'ok',
+            'results'=> $project
+        ]);
+    }else{
+        return response()->json([
+            'status'=> 'success',
+            'message' =>'error'
+            ]);
+    }
+
+}
+}
